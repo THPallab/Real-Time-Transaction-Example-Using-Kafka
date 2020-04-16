1696279cdd42b88e5b12e0c1595b6940cb3b985b
Md. Tanvir Hossain <tanvir.h.pallab@gmail.com>
2020-04-16 11:38:30 +0600

    Add files via upload
    Another producer script works when producer and consumer works on different machine.

---
+++ producer_for_different_machine.py
@@ -0,0 +1,36 @@
+from confluent_kafka import Producer
+import socket
+
+while (1):
+    def acked(err, msg):
+        if err is not None:
+            print("Failed to deliver message: {0}: {1}"
+                  .format(msg.value(), err.str()))
+        else:
+            print("Message produced: {0}".format(msg.value()))
+
+    p = Producer({'bootstrap.servers': '192.168.1.107:9092'})
+
+    try:
+            host='192.168.1.104' #client ip
+            port = 9092
+            server = ('192.168.1.107', 9092)    #server ip
+
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.bind((host,port))
+
+            # while (1):
+            #     message = input("-> ")
+            #     s.sendto(message.encode('utf-8'), server)
+            # s.close()
+
+            tt = input()
+            p.produce('testtopic','{0}'
+                      .format(tt), callback=acked)
+            p.poll(0.5)
+
+    except KeyboardInterrupt:
+        pass
+
+    p.flush(30)
+    s.close
